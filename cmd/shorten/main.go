da1cbe3b8eff2440493b5c6682b08ef755c1223f
Aurélien Rainone <aurelien.rainone@gmail.com>
2021-06-08 17:21:43 +0200

    cmd/shorten: handlers are now methods of server [2/2]

--- cmd/shorten/main.go
+++ cmd/shorten/main.go
@@ -32,14 +32,70 @@ func (s *ShortenArgs) validate() error {
 
 type server struct {
 	shortener *shorten.URLShortener
+	router    *http.ServeMux
 }
 
 func newServer(store shorten.Store) *server {
 	s := &server{
 		shortener: shorten.NewURLShortener(store),
+		router:    http.NewServeMux(),
+	}
+	s.routes()
+	return s
+}
+
+func (s *server) routes() {
+	s.router.HandleFunc("/v1/shorten", s.handleShorten)
+	s.router.HandleFunc("/", s.handleIndex)
+}
+
+func (s *server) handleShorten(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Println(r.URL.Path)
+		fmt.Fprintln(w, "/v1/shorten: wrong method", r.Method)
+		log.Println("/v1/shorten: wrong method", r.Method)
+		return
 	}
 
-	return s
+	var args ShortenArgs
+
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+	if err := dec.Decode(&args); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, "/v1/shorten: invalid json:", err)
+		log.Println("/v1/shorten: invalid json:", err)
+		return
+	}
+
+	if err := args.validate(); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, "/v1/shorten: failed arg validation:", err)
+		log.Println("/v1/shorten: failed arg validation:", err)
+		return
+	}
+
+	shortURL := s.shortener.Shorten(args.LongURL)
+	fmt.Fprintln(w, shortURL)
+}
+
+func (s *server) handleIndex(w http.ResponseWriter, r *http.Request) {
+	shortURL := strings.TrimPrefix(r.URL.Path, "/")
+	longURL, ok := s.shortener.Long(shortURL)
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintln(w, r.URL.Path, "not found")
+		log.Println(r.URL.Path, "not found")
+		return
+	}
+
+	log.Println(r.URL.Path, "redirecting to", longURL)
+	http.Redirect(w, r, longURL, http.StatusMovedPermanently)
+}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.router.ServeHTTP(w, r)
 }
 
 func main() {
@@ -49,52 +105,7 @@ func main() {
 
 	s := newServer(shorten.NewMemoryStore())
 
-	http.HandleFunc("/v1/shorten", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != "POST" {
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			fmt.Println(r.URL.Path)
-			fmt.Fprintln(w, "/v1/shorten: wrong method", r.Method)
-			log.Println("/v1/shorten: wrong method", r.Method)
-			return
-		}
-
-		var args ShortenArgs
-
-		dec := json.NewDecoder(r.Body)
-		dec.DisallowUnknownFields()
-		if err := dec.Decode(&args); err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprintln(w, "/v1/shorten: invalid json:", err)
-			log.Println("/v1/shorten: invalid json:", err)
-			return
-		}
-
-		if err := args.validate(); err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprintln(w, "/v1/shorten: failed arg validation:", err)
-			log.Println("/v1/shorten: failed arg validation:", err)
-			return
-		}
-
-		shortURL := s.shortener.Shorten(args.LongURL)
-		fmt.Fprintln(w, shortURL)
-	})
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		shortURL := strings.TrimPrefix(r.URL.Path, "/")
-		longURL, ok := s.shortener.Long(shortURL)
-		if !ok {
-			w.WriteHeader(http.StatusNotFound)
-			fmt.Fprintln(w, r.URL.Path, "not found")
-			log.Println(r.URL.Path, "not found")
-			return
-		}
-
-		log.Println(r.URL.Path, "redirecting to", longURL)
-		http.Redirect(w, r, longURL, http.StatusMovedPermanently)
-	})
-
-	if err := http.ListenAndServe(addrFlag, nil); err != nil {
+	if err := http.ListenAndServe(addrFlag, s); err != nil {
 		log.Fatalf("ListenAndServe: %v", err)
 	}
 }

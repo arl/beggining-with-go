77ae17fe34e916fee345cd2731705857905bf5a5
Aurélien Rainone <aurelien.rainone@gmail.com>
2021-06-09 19:25:26 +0200

    cmd/shorten: handle os.Interrupt

--- cmd/shorten/main.go
+++ cmd/shorten/main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 
 	"github.com/arl/shorten"
 )
@@ -15,7 +17,15 @@ func main() {
 
 	s := newServer(shorten.NewMemoryStore())
 
-	if err := http.ListenAndServe(addrFlag, s); err != nil {
-		log.Fatalf("ListenAndServe: %v", err)
-	}
+	go func() {
+		if err := http.ListenAndServe(addrFlag, s); err != nil {
+			log.Fatalf("ListenAndServe: %v", err)
+		}
+	}()
+	log.Println("listening on", addrFlag)
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+	<-ch
+	log.Println("quitting...")
 }
